bd962c57272c1b85fc0e89d52e553c37a0862455
Siham Kassim <152332445+Sihamkassim@users.noreply.github.com>
2026-02-20 16:53:56 +0300

    Comment out alternative romanToInt implementation
    Commented out an alternative implementation of the romanToInt function.

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -19,3 +19,17 @@ class Solution:
                 total += roman[s[i]]
 
         return total
+# or 
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         roman = {"I":1, "V":5,"X": 10, "L":50, "C":100, "D":500, "M":1000}
+
+#         last = 0
+#         res = 0
+#         for i in s[::-1]:
+#             if roman[i] >= last:
+#                 res += roman[i]
+#                 last = roman[i]
+#             else:
+#                 res -= roman[i]
+#         return res
