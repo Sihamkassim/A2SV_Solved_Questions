2b9f30210e9e027a2588cdf052dbdcce36ac6790
Siham Kassim <152332445+Sihamkassim@users.noreply.github.com>
2026-03-06 21:59:57 +0300

    Add solution for Pancake Sorting

---
+++ leetcode/0969-pancake-sorting/0969-pancake-sorting.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        res = []
+        n = len(arr)
+
+        for size in range(n, 1, -1):
+            max_index = arr.index(size)
+
+            if max_index == size - 1:
+                continue
+
+            if max_index != 0:
+                res.append(max_index + 1)
+                arr[:max_index + 1] = reversed(arr[:max_index + 1])
+
+            res.append(size)
+            arr[:size] = reversed(arr[:size])
+
+        return res
